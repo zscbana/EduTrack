7eb61105b76a6323cd8bf2b2013713b862daeb54
zscbana <zscbana@gmail.com>
2024-05-02 16:22:58 +0300

    main page

---
+++ main.js
@@ -0,0 +1,23 @@
+let stars = document.getElementById('stars');
+let Edutrack = document.querySelector(`.Edutrack`);
+window.onscroll = function(){
+     let value = scrollY;
+     stars.style.left = value + 'px';
+     Edutrack.style.fontSize = value + 'px';
+     if (scrollY >= 113){
+        Edutrack.style.fontSize = 154 + 'px';
+        Edutrack.style.position = 'fixed';
+        if(scrollY >= 220){
+            Edutrack.style.display = 'none';
+        }
+        else{
+            Edutrack.style.display = 'block';
+        }
+        if(scrollY >= 114){
+        document.querySelector('.main').style.background = 'linear-gradient(black, rgb(33, 32, 32))';
+        }
+        else{
+        document.querySelector('.main').style.background = 'linear-gradient(rgb(34, 100, 113),rgb(33, 32, 32))';
+        }
+     }
+}
